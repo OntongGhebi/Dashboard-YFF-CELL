88231c007f7e59c8da5b826647882a44bb021ee2
OntongGhebi <ghebiarmando@gmail.com>
2025-02-22 22:01:32 +0700

    Budget, Savings, Subscriptions, Report dan 1/2 Loans

--- dashboard-yff/src/components/ContentMain/ContentMain.jsx
+++ dashboard-yff/src/components/ContentMain/ContentMain.jsx
@@ -2,6 +2,12 @@ import Cards from "../Cards/Cards";
 import Transactions from "../Transactions/Transactions";
 import Report from "../Report/Report";
 import "./ContentMain.css";
+import Budget from "../Budget/Budget";
+import Subscriptions from "../Subscriptions/Subscriptions";
+import Savings from "../Savings/Savings";
+import Loans from "../Loans/Loans";
+import Financial from "../Financial/Financial";
+
 
 const ContentMain = () => {
   return (
@@ -11,7 +17,22 @@ const ContentMain = () => {
             <Transactions />
             <Report />
         </div>
-        <div className="content-grid-two"></div>
+        <div className="content-grid-two">
+            <Budget />
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                  <Subscriptions />
+                  <Savings />
+              </div>
+            </div>
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                  <Loans />
+                  <Financial />
+
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
